e6e2583486a3c95d3cd66d6377f609dab428df47
z1V-Chan <71861767+z1V-Chan@users.noreply.github.com>
2022-01-12 23:25:15 +0800

    test

--- src/models/orignal/SLRC_BPR.py
+++ src/models/orignal/SLRC_BPR.py
@@ -17,9 +17,10 @@ from common.constants import *
 from common.reader import Reader
 from models.SLRC import SLRC
 
+
 class SLRC_BPR(SLRC):
     def _init_hawks_weights(self):
-        self.global_alpha = nn.Parameter(torch.tensor(1.))
+        self.global_alpha = nn.Parameter(torch.tensor(1.0))
         self.alpha = nn.Embedding(self.item_num, 1)
         self.pi = nn.Embedding(self.item_num, 1)
         self.beta = nn.Embedding(self.item_num, 1)
@@ -35,33 +36,35 @@ class SLRC_BPR(SLRC):
         :param feed_dict:
         :return:
         """
-        items = feed_dict['item_id']  # the 1st is tested and other is neg
-        time_interval = feed_dict['time_intervals']/TIME_SCALE
-        mask=(time_interval>0).float()
+        items = feed_dict["item_id"]  # the 1st is tested and other is neg
+        time_interval = feed_dict["time_intervals"] / TIME_SCALE
+        mask = (time_interval > 0).float()
 
         alpha_b = self.alpha(items)
-        alpha = (self.global_alpha).clamp(min=1e-10,max=10) + alpha_b
-        beta = (self.beta(items) + 1).clamp(min=1e-10,max=10)
+        alpha = (self.global_alpha).clamp(min=1e-10, max=10) + alpha_b
+        beta = (self.beta(items) + 1).clamp(min=1e-10, max=10)
         pi = self.pi(items) + 0.5
-        mu = self.mu(items) + 1 # may fix later
-        sigma = (self.sigma(items) + 1).clamp(min=1e-10,max=10)
-        exp=exponential.Exponential(beta,validate_args=False)
-        norm=normal.Normal(mu,sigma,validate_args=False)
+        mu = self.mu(items) + 1  # may fix later
+        sigma = (self.sigma(items) + 1).clamp(min=1e-10, max=10)
+        exp = exponential.Exponential(beta, validate_args=False)
+        norm = normal.Normal(mu, sigma, validate_args=False)
 
-        dt=time_interval
-        gamma1=pi*(exp.log_prob(dt).exp())
-        gamma2=(1-pi)*(norm.log_prob(dt).exp())
-        gamma=gamma1+gamma2
-        excit1=alpha*gamma
-        excit2=excit1*mask
-        excit= (excit2).sum(-1)
+        dt = time_interval
+        gamma1 = pi * (exp.log_prob(dt).exp())
+        gamma2 = (1 - pi) * (norm.log_prob(dt).exp())
+        gamma = gamma1 + gamma2
+        excit1 = alpha * gamma
+        excit2 = excit1 * mask
+        excit = (excit2).sum(-1)
         return excit
 
     def _forward_CF(self, feed_dict):
-        users = feed_dict['user_id']
-        items = feed_dict['item_id']
+        users = feed_dict["user_id"]
+        items = feed_dict["item_id"]
         u_vector = self.user_embed(users)
         i_vector = self.item_embed(items)
 
-        base = (u_vector[:, None, :] * i_vector).sum(-1)  # the dim of users and items is not the same
+        base = (u_vector[:, None, :] * i_vector).sum(
+            -1
+        )  # the dim of users and items is not the same
         return base
